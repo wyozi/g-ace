69dc6c5042995f2eb3c68edf399d26e571d08be7
wyozi <joonasmertanen@gmail.com>
2014-02-21 17:44:22 +0200

    Add mode changer

--- lua/autorun/client/cl_editor.lua
+++ lua/autorun/client/cl_editor.lua
@@ -200,6 +200,35 @@ gace.TitleBarComponents = {
 			menu:Open()
 		end
 	},
+	{
+		text = "Mode",
+		fn = function()
+			local menu = DermaMenu()
+
+			local modes = {
+				"abap", "actionscript", "ada", "apache_conf", "asciidoc", "assembly_x86", "autohotkey",
+				"batchfile", "c9search", "c_cpp", "clojure", "cobol", "coffee", "coldfusion", "csharp",
+				"css", "curly", "d", "dart", "diff", "django", "dot", "ejs", "erlang", "forth", "ftl",
+				"glsl", "glua", "golang", "groovy", "haml", "handlebars", "haskell", "haxe", "html", "ini",
+				"jack", "jade", "java", "javascript", "json","jsoniq", "jsp", "jsx", "julia", "latex", "less",
+				"liquid", "lisp", "livescript", "logiql", "lsl", "lua", "luapage", "lucene", "makefile", "markdown",
+				"matlab", "mel", "mushcode", "mysql", "nix", "objectivec", "ocaml", "pascal", "perl", "pgsql", "php",
+				"plain_text", "powershell", "prolog", "properties", "protobuf", "python", "r", "rdoc", "rhtml", "ruby",
+				"rust", "sass", "scad", "scala", "scheme", "scss", "sh", "sjs", "snippets", "soy_template", "space",
+				"sql", "stylus", "svg", "tcl", "tex", "text", "textile", "tmsnippet", "toml", "twig", "typescript",
+				"vbscript", "velocity", "verilog", "vhdl", "xml", "xquery", "yaml",
+			}
+
+			for _,mode in pairs(modes) do
+				local mode2 = "ace/mode/" .. mode
+				local opt = menu:AddOption(mode, function() gace.RunEditorJS("editor.getSession().setMode('" .. mode2 .. "')") end)
+				if gace.OpenedSessionMode == mode2 then
+					opt:SetChecked(true)
+				end
+			end
+			menu:Open()
+		end
+	},
 }
 
 surface.CreateFont("EditorTabFont", {
@@ -301,7 +330,7 @@ end
 function gace.CreateHTMLPanel()
 	local html = vgui.Create("DHTML")
 
-	html:AddFunction("gace", "SetOpenedSession", function(id, content)
+	html:AddFunction("gace", "SetOpenedSession", function(id, content, mode)
 		gace.OpenedSessionId = id
 		gace.OpenedSessionContent = content
 
@@ -381,6 +410,10 @@ function gace.CreateHTMLPanel()
 		gace.UIColors.tab_fg = RGBStringToColor(fgColor)
 
 		cookie.Set("gace-theme", theme)
+	end)
+	
+	html:AddFunction("gace", "ModeChanged", function(mode)
+		gace.OpenedSessionMode = mode
 	end)
 
 	html:AddFunction("gace", "EditorReady", function()
