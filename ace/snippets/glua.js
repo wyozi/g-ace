4cc0fb296de217c48b5f9b5e42c7958eda9a42c7
wyozi <joonasmertanen@gmail.com>
2014-08-16 13:37:12 +0300

    Update glua snippets
    Make for snippets use tab instead of x amount of spaces to support multiple tab sizes

--- ace/snippets/glua.js
+++ ace/snippets/glua.js
@@ -8,7 +8,7 @@ snippet local\n\
 	local ${1:x} = ${2:1}\n\
 snippet fun\n\
 	function ${1:fname}(${2:...})\n\
-		${3:-- body}\n\
+	\t${3:-- body}\n\
 	end\n\
 snippet anonfun\n\
 	function(${2:...})\n\
@@ -16,15 +16,15 @@ snippet anonfun\n\
 	end\n\
 snippet for\n\
 	for ${1:i}=${2:1},${3:10} do\n\
-		${4:print(i)}\n\
+	\t${4:print(i)}\n\
 	end\n\
 snippet forp\n\
 	for ${1:i},${2:v} in pairs(${3:table_name}) do\n\
-	   ${4:-- body}\n\
+	\t${4:-- body}\n\
 	end\n\
 snippet fori\n\
 	for ${1:i},${2:v} in ipairs(${3:table_name}) do\n\
-	   ${4:-- body}\n\
+	\t${4:-- body}\n\
 	end\n\
 ";
 exports.scope = "glua";
