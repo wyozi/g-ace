7e09c4a7c2de32b871fc198d7a622547cdf37c02
wyozi <joonasmertanen@gmail.com>
2014-08-18 16:23:12 +0300

    Add base g-ace extension system

---
+++ lua/autorun/gace_ext.lua
@@ -0,0 +1,17 @@
+gace.Extensions = {}
+
+local cl_extensions = file.Find("gace-ext/cl/*.lua", "LUA")
+local sh_extensions = file.Find("gace-ext/sh/*.lua", "LUA")
+local sv_extensions = file.Find("gace-ext/sv/*.lua", "LUA")
+
+for _,ext in pairs(cl_extensions) do
+	if SERVER then AddCSLuaFile("gace-ext/cl/" .. ext) end
+	if CLIENT then include("gace-ext/cl/" .. ext) end
+end
+for _,ext in pairs(sh_extensions) do
+	if SERVER then AddCSLuaFile("gace-ext/sh/" .. ext) end
+	include("gace-ext/sh/" .. ext)
+end
+for _,ext in pairs(sv_extensions) do
+	if SERVER then include("gace-ext/sv/" .. ext) end
+end
