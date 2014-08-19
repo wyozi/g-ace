09fbf8e03bb025239e55c40082ef30f910e592a8
wyozi <joonasmertanen@gmail.com>
2014-08-19 17:09:41 +0300

    Add html funcs for theme and mode changes

--- lua/autorun/client/cl_htmlfuncs.lua
+++ lua/autorun/client/cl_htmlfuncs.lua
@@ -27,6 +27,16 @@ function gace.SetHTMLSession(id, content, requestDataIfNotCached)
 	
 end
 
+gace.AvailableThemes = {
+	"ambiance", "chaos", "chrome", "clouds", "clouds_midnight", "cobalt",
+	"crimson_editor", "dawn", "dreamweaver", "eclipse", "github", "idle_fingers",
+	"katzenmilch", "kr", "kuroir", "merbivore", "merbivore_soft", "mono_industrial",
+	"monokai", "pastel_on_dark", "solarized_dark", "solarized_light", "terminal",
+	"textmate", "tomorrow", "tomorrow_night", "tomorrow_night_blue",
+	"tomorrow_night_bright", "tomorrow_night_eighties", "twilight",
+	"vibrant_ink", "xcode",
+}
+
 gace.AddHook("SetupHTMLPanel", "Editor_SetupHTMLFunctions", function(html)
 	-- Session related functions
 	html:AddFunction("gace", "UpdateSessionContent", function(content)
@@ -60,4 +70,31 @@ gace.AddHook("SetupHTMLPanel", "Editor_SetupHTMLFunctions", function(html)
 	end)
 
 	-- General editor related functions (such as updating theme)
+	html:AddFunction("gace", "EditorReady", function()
+		local c_theme = cookie.GetString("gace-theme", "ace/theme/tomorrow_night") or "ace/theme/tomorrow_night"
+		local the_theme = "ace/theme/tomorrow_night"
+		if table.HasValue(gace.AvailableThemes, c_theme:Split("/")[3]) then
+			the_theme = c_theme
+		end
+		gace.RunJavascript("editor.setTheme('" .. the_theme .. "')")
+	end)
+
+	local function RGBStringToColor(str)
+		local r, g, b = string.match(str, "(%d+):(%d+):(%d+)")
+		return Color(tonumber(r), tonumber(g), tonumber(b))
+	end
+	html:AddFunction("gace", "ThemeChanged", function(theme, bgColor, fgColor, gutterBgColor)
+		gace.UIColors.frame_bg = RGBStringToColor(bgColor)
+		gace.UIColors.frame_fg = RGBStringToColor(fgColor)
+
+		gace.UIColors.tab_bg = RGBStringToColor(gutterBgColor)
+		gace.UIColors.tab_fg = RGBStringToColor(fgColor)
+
+		cookie.Set("gace-theme", theme)
+	end)
+
+	html:AddFunction("gace", "ModeChanged", function(mode)
+		local sess = gace.GetOpenSession()
+		if sess then sess.mode = mode end
+	end)
 end)
