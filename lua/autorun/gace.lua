8c70374e8e493a0a73a67e7e9cc0eb8f80e593d2
wyozi <joonasmertanen@gmail.com>
2014-12-15 18:48:44 +0200

    Move gace debug to gace_logging.lua

--- lua/autorun/gace.lua
+++ lua/autorun/gace.lua
@@ -1,15 +1,5 @@
 gace = gace or {}
 
-local debug_cvar = CreateConVar("gace_debug", "0")
-function gace.IsDebug()
-	return debug_cvar:GetBool()
-end
-function gace.Debug(...)
-	if gace.IsDebug() then
-		MsgN("GACE DEBUG: ", ...)
-	end
-end
-
 -- Include testing stuff
 if SERVER then AddCSLuaFile("tests/_gacetests.lua") end
 include("tests/_gacetests.lua")
--- lua/autorun/gace_logging.lua
+++ lua/autorun/gace_logging.lua
@@ -1,7 +1,7 @@
 -- Default logging mechanism. Simply concats passed arguments converting them to string using "tostring()" if needed
 -- "LogMessage" GAce hook can override the whole log system
 gace.Log = function(...)
-	
+
 	if gace.CallHook("LogMessage", ...) then return end
 
 	local msgtbl = {}
@@ -10,4 +10,14 @@ gace.Log = function(...)
 	end
 
 	MsgN("[G-Ace Log] " .. table.concat(msgtbl, ""))
+end
+
+local debug_cvar = CreateConVar("gace_debug", "0")
+function gace.IsDebug()
+	return debug_cvar:GetBool()
+end
+function gace.Debug(...)
+	if gace.IsDebug() then
+		gace.Log("[Debug] ", ...)
+	end
 end
