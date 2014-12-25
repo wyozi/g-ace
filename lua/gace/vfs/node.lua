6445806759fc0ce058628d6f647581a15ea7cdb8
Wyozi <joonasmertanen@gmail.com>
2014-12-25 20:02:06 +0200

    Add getParent() and path()

--- lua/gace/vfs/node.lua
+++ lua/gace/vfs/node.lua
@@ -5,4 +5,21 @@ local Node = gace.VFS.Node
 Node:include(gace.EventEmitter)
 
 function Node:getName()
+    gace.Error(string.format("%s#%s is not implemented", self.class.name, "getName"))
+end
+
+function Node:getParent()
+    return self.parent
+end
+
+function Node:path()
+    local components = {self:getName()}
+
+    local par = self:getParent()
+    while par do
+        table.insert(components, 1, par:getName())
+        par = par:getParent()
+    end
+
+    return table.concat(components, "/")
 end
