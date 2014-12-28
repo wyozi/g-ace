51aba746f8aff96baa92b04db14dceb316119a75
wyozi <joonasmertanen@gmail.com>
2014-12-28 15:24:20 +0200

    Make 'esc' close userinput if it's open

--- lua/gace-ext/cl/editor_hotkeys.lua
+++ lua/gace-ext/cl/editor_hotkeys.lua
@@ -1,4 +1,16 @@
--- Make it so pressing esc while editor is open hides the editor
+-- Handle overriding escape key.
+--
+-- If you want to do something when esc is pressed, call gace.ext.PushESCListener
+-- with a callback function. Callback will be called once and then removed
+--
+-- If you return 'false' from the callback, callback will be dismissed as if it
+-- never existed and will immediately test the next callback
+
+local esc_listener_stack = {}
+function gace.ext.PushESCListener(listener)
+	table.insert(esc_listener_stack, listener)
+end
+
 local was_esc_down
 gace.AddHook("FrameThink", "Hotkey_ESCToHide", function(frame)
 	local is_esc_down = input.IsKeyDown(KEY_ESCAPE)
@@ -14,13 +26,24 @@ gace.AddHook("FrameThink", "Hotkey_ESCToHide", function(frame)
 			end
 		end
 
-		--if gace.InputPanel:IsVisible() then
-		--	gace.InputPanel:Hide()
-		--	gace.Frame:InvalidateLayout()
-		--	CancelGUIOpen()
-		--elseif gaceclosewithesc:GetBool() then
+		local esc_overridden = false
+
+		-- Keep popping esc_listeners until we get something that returns ~=false
+		while true do
+			local popped = table.remove(esc_listener_stack, #esc_listener_stack)
+			if not popped then break end
+
+			local ret = popped()
+			if ret ~= false then
+				esc_overridden = true
+				break
+			end
+		end
+
+		-- If esc wasnt overridden by extensions, we use it for hiding the frame
+		if not esc_overridden then
 			frame:SetVisible(false)
-			CancelGUIOpen()
-		--end
+		end
+		CancelGUIOpen()
 	end
 end)
