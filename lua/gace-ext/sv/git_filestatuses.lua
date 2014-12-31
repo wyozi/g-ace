3cae4f0434c23610bed8e5821a32f7e77cde9c0f
wyozi <joonasmertanen@gmail.com>
2014-12-31 23:56:08 +0200

    Fix warning on saving on non-git folders. Fixes #41

--- lua/gace-ext/sv/git_filestatuses.lua
+++ lua/gace-ext/sv/git_filestatuses.lua
@@ -5,30 +5,31 @@ function gace.GitBroadcastRepoStatus(ply, path)
         return
     end
 
-	gace.git.virt_to_real(path):then_(function(realpath)
-		local status, err = gace.git.status(realpath)
+	gace.git.virt_to_real(path, false, true):then_(function(fsRootNode)
+		if not gace.git.is_repo(fsRootNode:realPath()) then return end
+
+        local status, err = gace.git.status(fsRootNode:realPath())
 		if not status then
 			MsgN("gace.GitBroadcastRepoStatus failed for '", path, "': ", err)
 			return
 		end
 
-		gace.git.virt_to_real(path, false, true):then_(function(fsRootNode)
-			local payload = {}
+		local payload = {}
 
-			--Same file can be in both IndexChganes and WDChanges, but WD takes
-			--priority so it needs to be after
+		--Same file can be in both IndexChganes and WDChanges, but WD takes
+		--priority so it needs to be after
 
-			for _,ic in pairs(status.IndexChanges) do
-				payload[fsRootNode:path() .. "/" .. ic.Path] = "i_" .. ic.Status:sub(1, 1)
-			end
+		for _,ic in pairs(status.IndexChanges) do
+			payload[fsRootNode:path() .. "/" .. ic.Path] = "i_" .. ic.Status:sub(1, 1)
+		end
 
-			for _,wdc in pairs(status.WorkDirChanges) do
-				payload[fsRootNode:path() .. "/" .. wdc.Path] = "wd_" .. wdc.Status:sub(1, 1)
-			end
+		for _,wdc in pairs(status.WorkDirChanges) do
+			payload[fsRootNode:path() .. "/" .. wdc.Path] = "wd_" .. wdc.Status:sub(1, 1)
+		end
 
-			local vfolder = gace.path.head(path)
-			gace.NetMessageOut(0, "git_updstatus", {vfolder = vfolder, changes = payload}):Send(ply)
-		end)
+		local vfolder = gace.path.head(path)
+		gace.NetMessageOut(0, "git_updstatus", {vfolder = vfolder, changes = payload}):Send(ply)
+
 
 	end):catch(print)
 end
