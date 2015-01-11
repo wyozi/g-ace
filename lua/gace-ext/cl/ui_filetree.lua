74395383079450b28c388a95d1a2f092ac1c9fa5
wyozi <joonasmertanen@gmail.com>
2015-01-11 23:28:58 +0200

    Remove filetreemeta stuff. Doesn't work

--- lua/gace-ext/cl/ui_filetree.lua
+++ lua/gace-ext/cl/ui_filetree.lua
@@ -5,18 +5,8 @@ gace.AddHook("AddPanels", "Editor_AddFileTree", function(frame, basepnl)
 
 	local filetree = vgui.Create("GAceTree")
 
-	local filetreemeta = gace.ClientCache:get("filetreemeta")
-	if filetreemeta then
-		gace.FileNodeTreeMeta = filetreemeta
-	end
-
 	-- Requests the server to update the whole filetree immediately
 	gace.filetree.RefreshPath(gace.GetOption("root_path"))
-
-	-- Stores filetree meta to local filecache every 10seconds
-	timer.Create("GAceFiletreeMetaPersistance", 10, 0, function()
-		gace.ClientCache:set("filetreemeta", gace.FileNodeTreeMeta)
-	end)
 
 	sb:AddDocked("FileTree", filetree, FILL)
 end)
