6e928caea1914c254bdac5eae1a32da93f02daf3
wyozi <joonasmertanen@gmail.com>
2015-01-15 22:06:40 +0200

    Add protocol messagesize estimation

--- lua/gace/netmsg/protocol_netlib.lua
+++ lua/gace/netmsg/protocol_netlib.lua
@@ -6,11 +6,30 @@ gace.AddHook("HandleNetMessage", "_ResponseToListeners", function(packet)
     end
 end)
 
-gace.NetMessageDefaultProtocol = {
-    Send = function(netmsg)
-        gace.SendNetMessage(netmsg)
-    end,
-    Listen = function(netmsg, callback)
-        gace.NetMsgListeners[netmsg:GetReqId()] = callback
+gace.NetMessageDefaultProtocol = {}
+local protocol = gace.NetMessageDefaultProtocol
+
+function protocol.Send(netmsg)
+    gace.SendNetMessage(netmsg)
+end
+function protocol.Listen(netmsg, callback)
+    gace.NetMsgListeners[netmsg:GetReqId()] = callback
+end
+
+function protocol.EstimateMessageSize(payload)
+    local t = type(payload)
+
+    if t == "table" then
+        local size = 0
+        for k, v in pairs(payload) do
+            size = size + protocol.EstimateMessageSize(k)
+            size = size + protocol.EstimateMessageSize(v)
+        end
+        return size
+    elseif t == "string" then
+        return #t -- lua is "nice" and returns size in bytes instead of individual chars
     end
-}
+
+    -- this is an estimation, we dont really care about few byte differences
+    return 4
+end
