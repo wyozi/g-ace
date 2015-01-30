f677321d9cb485e65dd3dba77dee1542c4c9434d
wyozi <joonasmertanen@gmail.com>
2015-01-30 18:17:20 +0200

    Add pretty log colors

--- lua/gace/util/logging.lua
+++ lua/gace/util/logging.lua
@@ -1,15 +1,22 @@
 -- Default logging mechanism. Simply concats passed arguments converting them to string using "tostring()" if needed
 -- "LogMessage" GAce hook can override the whole log system
 gace.Log = function(...)
-
 	if gace.CallHook("LogMessage", ...) then return end
 
-	local msgtbl = {}
+	MsgC(Color(78, 205, 196), "[G-Ace Log] ")
+
+	local clr
 	for _,v in pairs{...} do
-		table.insert(msgtbl, tostring(v))
+		if type(v) == "table" and v.r and v.g and v.b then
+			clr = v
+		elseif clr then
+			MsgC(clr, tostring(v))
+		else
+			Msg(tostring(v))
+		end
 	end
 
-	MsgN("[G-Ace Log] " .. table.concat(msgtbl, ""))
+	MsgN()
 end
 
 local debug_cvar = CreateConVar("gace_debug", "0")
