5fe4885ebfb0244dcd8b30208e181ef922cda345
wyozi <joonasmertanen@gmail.com>
2015-02-06 18:54:28 +0200

    Remove luadev dependency on SWEP and SENT. Fixes #69

--- lua/gace-ext/cl/ui_actionbar_luarun_ent.lua
+++ lua/gace-ext/cl/ui_actionbar_luarun_ent.lua
@@ -1,5 +1,16 @@
 gace.AddHook("AddActionBarComponents", "ActionBar_LuaRun_Ents", function(comps)
 	comps:AddCategory("Run as", Color(39, 174, 96))
+
+	local function CreateRequest(op, code)
+		gace.SendRequest(op, {code = code}, function(_, _, pl)
+			if pl.err then
+				gace.Log(gace.LOG_ERROR, op .. " failed: ", pl.err)
+			else
+				gace.Log(op .. " done!")
+			end
+		end)
+	end
+
 	comps:AddComponent {
 		text = "SWEP",
 		fn = function(state)
@@ -11,12 +22,9 @@ SWEP.Secondary = {}
 weapons.Register(SWEP, "%s", true)
 			]]
 
-			luadev.RunOnShared(
-				string.format(base, gace.GetOpenSession().Content, gace.GetOpenSession():GetExtensionlessName()),
-				"g-ace swep: " .. (gace.GetSessionId() or "")
-			)
+			CreateRequest("lua-runsh", string.format(base, gace.GetOpenSession().Content, gace.GetOpenSession():GetExtensionlessName()))
 		end,
-		enabled = function() return luadev ~= nil and gace.IsSessionOpen() end,
+		enabled = function() return gace.IsSessionOpen() end,
 		tt = "Runs the code as SWEP. SWEP name is equal to extensionless file name"
 	}
 	comps:AddComponent {
@@ -28,12 +36,9 @@ local ENT = {}
 scripted_ents.Register(ENT, "%s")
 			]]
 
-			luadev.RunOnShared(
-				string.format(base, gace.GetOpenSession().Content, gace.GetOpenSession():GetExtensionlessName()),
-				"g-ace sent: " .. (gace.GetSessionId() or "")
-			)
+			CreateRequest("lua-runsh", string.format(base, gace.GetOpenSession().Content, gace.GetOpenSession():GetExtensionlessName()))
 		end,
-		enabled = function() return luadev ~= nil and gace.IsSessionOpen() end,
+		enabled = function() return gace.IsSessionOpen() end,
 		tt = "Runs the code as SENT. SENT name is equal to extensionless file name"
 	}
 	comps:AddCategoryEnd()
