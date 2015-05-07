bd7f63ba885427030e2e189fb5a9eb623cad8a1e
wyozi <joonasmertanen@gmail.com>
2015-05-07 14:43:43 +0300

    Use brighter colors on hover/depress for tab_bg overriden buttons

--- lua/gace/vgui/vgui_gacebutton.lua
+++ lua/gace/vgui/vgui_gacebutton.lua
@@ -1,3 +1,7 @@
+local function HSVMod(clr, h, s, v)
+	local _h, _s, _v = ColorToHSV(clr)
+	return HSVToColor((_h+h)%360, math.Clamp(_s+s, 0, 1), math.Clamp(_v+v, 0, 1))
+end
 
 local VGUI_BUTTON = {
 	QueryColor = function(self, clrid)
@@ -9,19 +13,32 @@ local VGUI_BUTTON = {
 		self.Colors = self.Colors or {}
 		self.Colors[clrid] = clr
 	end,
-	Paint = function(self, w, h)
-		local colors = self.Colors
+	IsColorOverridden = function(self, clrid)
+		return self.Colors ~= nil and self.Colors[clrid] ~= nil
+	end,
+
+	GetBGColor = function(self)
 		if self:GetDisabled() then
-			surface.SetDrawColor(127, 110, 110)
-		elseif self.Depressed then
-			surface.SetDrawColor(self:QueryColor("tab_bg_active"))
-		elseif self.Hovered then
-			surface.SetDrawColor(self:QueryColor("tab_bg_hover"))
-		elseif self.ToggleMode and self.Toggled then
-			surface.SetDrawColor(self:QueryColor("tab_bg_active"))
-		else
-			surface.SetDrawColor(self:QueryColor("tab_bg"))
+			return Color(127, 110, 110)
 		end
+
+		local bg_overridden = self:IsColorOverridden("tab_bg")
+		if self.Depressed then
+			if bg_overridden then return HSVMod(self:QueryColor("tab_bg"), 0, 0, 0.25) end
+			return (self:QueryColor("tab_bg_active"))
+		elseif self.Hovered then
+			if bg_overridden then return HSVMod(self:QueryColor("tab_bg"), 0, 0, 0.15) end
+			return (self:QueryColor("tab_bg_hover"))
+		elseif self.ToggleMode and self.Toggled then
+			if bg_overridden then return HSVMod(self:QueryColor("tab_bg"), 0, 0, 0.25) end
+			return (self:QueryColor("tab_bg_active"))
+		end
+
+		return (self:QueryColor("tab_bg"))
+	end,
+	Paint = function(self, w, h)
+		local clr = self:GetBGColor()
+		surface.SetDrawColor(clr)
 		surface.DrawRect(0, 0, w, h)
 
 		local fg = self:QueryColor("tab_fg")
