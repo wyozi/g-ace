4a320ae84a296d65ebdb667d0aeafe54c1d6ee90
wyozi <joonasmertanen@gmail.com>
2016-07-26 20:46:55 +0300

    Move frame close check to post stack and refactor

--- lua/gace-ext/cl/editor_hotkeys.lua
+++ lua/gace-ext/cl/editor_hotkeys.lua
@@ -11,10 +11,14 @@ function gace.ext.PushESCListener(listener)
 	table.insert(esc_listener_stack, listener)
 end
 
--- List of ESC listeners that ALWAYS exist. "Pre" here means that these are evaluated _before_ normal esc_listener_stack.
+-- List of ESC listeners that ALWAYS exist. "Pre" here means that these are evaluated _before_ normal esc_listener_stack. "Post" means after.
 local static_pre_esc_listener_stack = {}
 function gace.ext.AddStaticPreESCListener(listener)
 	table.insert(static_pre_esc_listener_stack, listener)
+end
+local static_post_esc_listener_stack = {}
+function gace.ext.AddStaticPostESCListener(listener)
+	table.insert(static_post_esc_listener_stack, listener)
 end
 
 -- Add DMenu listener to post stack
@@ -33,6 +37,15 @@ gace.ext.AddStaticPreESCListener(function()
 	return false
 end)
 
+-- Close frame if it exists
+gace.ext.AddStaticPostESCListener(function()
+	if IsValid(gace.Frame) and gace.Frame:IsVisible() then
+		gace.Frame:Hide()
+		return true
+	end
+	return false
+end)
+
 local was_esc_down
 gace.AddHook("FrameThink", "Hotkey_ESCToHide", function(frame)
 	local is_esc_down = input.IsKeyDown(KEY_ESCAPE)
@@ -41,44 +54,53 @@ gace.AddHook("FrameThink", "Hotkey_ESCToHide", function(frame)
 
 	if esc_pressed then
 		local function CancelGUIOpen()
-			if gui.IsGameUIVisible () then
-				gui.HideGameUI ()
+			if gui.IsGameUIVisible() then
+				gui.HideGameUI()
 			else
-				gui.ActivateGameUI ()
+				gui.ActivateGameUI()
 			end
 		end
 
 		local esc_overridden = false
 
+		local function CheckStack(stack)
+			for i=#stack, 1, -1 do
+				local popped = stack[i]
+
+				local ret = popped()
+				if ret ~= false then
+					esc_overridden = true
+					break
+				end
+			end
+		end
+
 		-- Keep popping esc_listeners until we get something that returns ~=false
 
 		-- First check pre-stack
-		for i=#static_pre_esc_listener_stack, 1, -1 do
-			local popped = static_pre_esc_listener_stack[i]
+		CheckStack(static_pre_esc_listener_stack)
 
-			local ret = popped()
-			if ret ~= false then
-				esc_overridden = true
-				break
-			end
-		end
-
-		-- then check real stack
-		while true do
-			local popped = table.remove(esc_listener_stack, #esc_listener_stack)
-			if not popped then break end
-
-			local ret = popped()
-			if ret ~= false then
-				esc_overridden = true
-				break
-			end
-		end
-
-		-- If esc wasnt overridden by extensions, we use it for hiding the frame
+		-- then check real stack. This check is destructive (ie. pops from the stack)
 		if not esc_overridden then
-			frame:SetVisible(false)
+			while true do
+				local popped = table.remove(esc_listener_stack, #esc_listener_stack)
+				if not popped then break end
+
+				local ret = popped()
+				if ret ~= false then
+					esc_overridden = true
+					break
+				end
+			end
 		end
-		CancelGUIOpen()
+
+		-- Then check post stack
+		if not esc_overridden then
+			CheckStack(static_post_esc_listener_stack)
+		end
+
+		if esc_overridden then
+			CancelGUIOpen()
+		end
 	end
 end)
